db196df696cd1a28320a099f8674501ccccb632a
benjamin lin <blin432@gmail.com>
2018-09-18 16:29:20 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'This is Your Website ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'This is Your Website ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
